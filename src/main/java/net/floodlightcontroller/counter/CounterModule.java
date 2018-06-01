ed766553c75657f401ee3521a401901c56e56619
Davide Polonio <poloniodavide@gmail.com>
2018-06-01 16:51:01 +0200

    Counting IPv4 packets

--- src/main/java/net/floodlightcontroller/counter/CounterModule.java
+++ src/main/java/net/floodlightcontroller/counter/CounterModule.java
@@ -8,9 +8,15 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.packet.ARP;
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.MacAddress;
+import org.projectfloodlight.openflow.types.VlanVid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,13 +33,48 @@ public class CounterModule implements IOFMessageListener, IFloodlightModule {
     @Override
     public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
 
-        counter.incrementAndGet();
 
-        Ethernet eth =
-                IFloodlightProviderService.bcStore.get(cntx,
-                        IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+        switch (msg.getType()) {
+            case PACKET_IN:
+                /* Retrieve the deserialized packet in message */
+                Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
 
-        logger.info("Received a packet from " + eth.getSourceMACAddress().toString() + ". Packet count: " + counter.get());
+                /* Various getters and setters are exposed in Ethernet */
+                MacAddress srcMac = eth.getSourceMACAddress();
+                VlanVid vlanId = VlanVid.ofVlan(eth.getVlanID());
+
+                /*
+                 * Check the ethertype of the Ethernet frame and retrieve the appropriate payload.
+                 * Note the shallow equality check. EthType caches and reuses instances for valid types.
+                 */
+                if (eth.getEtherType() == EthType.IPv4) {
+                    /* We got an IPv4 packet; get the payload from Ethernet */
+                    IPv4 ipv4 = (IPv4) eth.getPayload();
+
+                    /* Various getters and setters are exposed in IPv4 */
+                    byte[] ipOptions = ipv4.getOptions();
+                    IPv4Address dstIp = ipv4.getDestinationAddress();
+
+                    /* Still more to come... */
+
+                    logger.info("Received a packet from " + dstIp.toString() + ". Packet count: " + counter.incrementAndGet());
+
+                } else if (eth.getEtherType() == EthType.ARP) {
+                    /* We got an ARP packet; get the payload from Ethernet */
+                    ARP arp = (ARP) eth.getPayload();
+
+                    /* Various getters and setters are exposed in ARP */
+                    boolean gratuitous = arp.isGratuitous();
+
+                    logger.info("Got an ARP message");
+
+                } else {
+                    /* Unhandled ethertype */
+                }
+                break;
+            default:
+                break;
+        }
 
         return Command.CONTINUE;
     }
