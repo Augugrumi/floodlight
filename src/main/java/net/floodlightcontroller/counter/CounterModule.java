93cb61e5fba919bead7943e2228621a46260a0d3
mzanella <zanna0150@gmail.com>
2018-06-05 11:15:54 +0200

    Modify counter module

--- src/main/java/net/floodlightcontroller/counter/CounterModule.java
+++ src/main/java/net/floodlightcontroller/counter/CounterModule.java
@@ -4,23 +4,22 @@ import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.internal.OFSwitch;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
-import net.floodlightcontroller.packet.ARP;
-import net.floodlightcontroller.packet.Ethernet;
-import net.floodlightcontroller.packet.IPv4;
+import net.floodlightcontroller.packet.*;
 import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFPacketOut;
 import org.projectfloodlight.openflow.protocol.OFType;
-import org.projectfloodlight.openflow.types.EthType;
-import org.projectfloodlight.openflow.types.IPv4Address;
-import org.projectfloodlight.openflow.types.MacAddress;
-import org.projectfloodlight.openflow.types.VlanVid;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.types.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -33,9 +32,42 @@ public class CounterModule implements IOFMessageListener, IFloodlightModule {
     @Override
     public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
 
-
         switch (msg.getType()) {
             case PACKET_IN:
+                Ethernet l2 = new Ethernet();
+                l2.setSourceMACAddress(MacAddress.of("00:00:00:00:00:01"));
+                l2.setDestinationMACAddress(MacAddress.BROADCAST);
+                l2.setEtherType(EthType.IPv4);
+
+                IPv4 l3 = new IPv4();
+                l3.setSourceAddress(IPv4Address.of("10.0.0.0"));
+                l3.setDestinationAddress(IPv4Address.of("10.0.0.3"));
+                l3.setTtl((byte) 64);
+                l3.setProtocol(IpProtocol.UDP);
+
+                UDP l4 = new UDP();
+                l4.setSourcePort(TransportPort.of(1));
+                l4.setDestinationPort(TransportPort.of(9090));
+
+                Data l7 = new Data();
+                l7.setData(new byte[1000]);
+
+                l2.setPayload(l3);
+                l3.setPayload(l4);
+                l4.setPayload(l7);
+
+                byte[] serializedData = l2.serialize();
+
+                OFPacketOut po = sw.getOFFactory().buildPacketOut() /* mySwitch is some IOFSwitch object */
+                        .setData(serializedData)
+                        .setActions(Collections.singletonList((OFAction) sw.getOFFactory().actions().output(OFPort.FLOOD, 0xffFFffFF)))
+                        .setInPort(OFPort.CONTROLLER)
+                        .build();
+
+                sw.write(po);
+
+                logger.warn("QUI");
+
                 /* Retrieve the deserialized packet in message */
                 Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
 
@@ -58,7 +90,7 @@ public class CounterModule implements IOFMessageListener, IFloodlightModule {
                     /* Still more to come... */
 
                     logger.info("Received a packet from " + ipv4.getSourceAddress() + "to " + ipv4.getDestinationAddress() +  ". Packet count: " + counter.incrementAndGet());
-                    logger.info("Packet protocol: " + ipv4.getProtocol().toString());
+                    //logger.info("Packet protocol: " + ipv4.getProtocol().toString());
 
                 } else if (eth.getEtherType() == EthType.ARP) {
                     /* We got an ARP packet; get the payload from Ethernet */
